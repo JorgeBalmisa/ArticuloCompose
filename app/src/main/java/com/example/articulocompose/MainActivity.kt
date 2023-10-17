7194096886b60fac995e881235b5a5594437f63f
JorgeBalmisa <palomaclase41@gmail.com>
2023-10-17 13:13:51 +0200

    Trabajo realizado en clase el 17/10/2023

--- app/src/main/java/com/example/articulocompose/MainActivity.kt
+++ app/src/main/java/com/example/articulocompose/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.articulocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.articulocompose.ui.theme.ArticuloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,10 +47,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+private fun Articulo(titulo : String, encabezado : String, descripcion : String, modifier : Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+
+        Text(
+            text = titulo,
+            fontSize = 88.sp,
+            modifier = modifier.padding(16.dp),
+            )
+        Text(
+            text = encabezado,
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp),
+        )
+        Text(
+            text = descripcion,
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp),
+        )
+    }
+
+
+
+
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArticuloComposeTheme {
-        Greeting("Android")
+        Articulo(titulo = stringResource(R.string.articuloTitulo), encabezado = stringResource(R.string.articuloEncabezado),
+            descripcion = stringResource(R.string.ArticuloDescripcion)
+        )
     }
 }
